f5440d9a0ef964e21798acbf0431628a83100675
kamalh2001 <63170492+kamalh2001@users.noreply.github.com>
2020-05-15 14:47:54 -0400

    added working structure for modes

--- GameModes.py
+++ GameModes.py
@@ -1,6 +1,8 @@
 import PokeStats
+adimport sys
 
 total_shown, one = 0, 1
+tries = 0
 
 
 def easy_mode(select, mode):
@@ -16,30 +18,40 @@ def easy_mode(select, mode):
 
 
 def medium_mode(select, mode):
-    global total_shown, one
+    global total_shown, one, tries
     one = 1
     three = 3
-    while one < 2:
+
+    while one < 2:  # might be unesscesary
         if len(select) == 0:
             print("out of poke")
+            # do something
             break
-        poke = PokeStats.Pokemon(select[0], mode) # select[0]
+        poke = PokeStats.Pokemon(select[0], mode)  # select[0]
         select.pop(0)
         print(poke.name)
         print(poke.idnum)
         print("test")
         guess = input("Guess a Pokemon\n")
-        while three < 4:
+        while 3 < 4:
             if guess.lower() == poke.name.lower():
-                print("correct generating new poke")
-                medium_mode(select, mode)
-                three += 1
+                tries += 1
+                winMessageAndNextMove(poke, select, mode)
+                print(next)
+
+            elif "hint" == guess.lower() != poke.name.lower():
+                print(poke.desc)
+                total_shown += 1
+                tries += 1
+                guess = input("Guess a Pokemon\n")
             elif guess.lower() != poke.name.lower():
                 print("wrong")
+                tries += 1
                 guess = input("Guess a Pokemon\n")
 
-        one +=1
+        one += 1
         poke = None
+
 
 def hard_mode(select):
     global total_shown, one
@@ -49,3 +61,21 @@ def hard_mode(select):
         guess = input("guess a pokemone\n")
         if guess == "lol":
             one += 1
+
+
+def winMessageAndNextMove(poke, select, mode):
+    modes = {"easy": easy_mode,
+             "medium": medium_mode,
+             "hard": hard_mode}
+    print(poke.name + ". Correct! It took you " + str(tries) + " tries.\n Get another Pokemon: Y\n Quit: N "
+                                                               "\nReset Game from the beginning: R")
+    response = input("Y/N \n")
+    if response == "Y":
+        modes[mode](select, mode)
+    elif response == "N":
+        print("Thank you for playing.")
+        sys.exit()
+    elif response == "R":
+        #reset game
+        pass
+    print("outside loop")
